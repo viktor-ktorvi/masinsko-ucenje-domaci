bf54a1f200d2ec21184e81b01d648748eba4d60c
Viktor <todosijevicviktor998@gmail.com>
2022-12-10 13:19:44 +0100

    refactor the train functions such that they take in kwargs that will be set depending on what method is used in the sweep

--- izbor_prediktora_stabla_ansambli/gradient_boosting.py
+++ izbor_prediktora_stabla_ansambli/gradient_boosting.py
@@ -3,7 +3,7 @@ import wandb
 
 import pandas as pd
 
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.metrics import accuracy_score
 
 from utils.validation import repeated_k_fold
@@ -19,31 +19,35 @@ def train_and_predict_foo(x_train, y_train, x_test, **kwargs):
     :param x_test: np.ndarray; shape num_test_samples x num_features; test dataset feature matrix
     :param n_estimators: int; number of models in the ensemble
     :param max_depth: int; maximum depth of decision trees
-    :param max_features: int; number of features to consider when looking for the best split
+    :param learning_rate: float; learning rate
     :param random_state: int; random seed for reproducibility purposes
     :return: np.ndarray; shape num_test_samples x 1; test predictions
     """
-    random_forest_clf = RandomForestClassifier(**kwargs)
-    random_forest_clf.fit(x_train, y_train.squeeze())
+    gradient_boosting_clf = GradientBoostingClassifier(**kwargs)
+    gradient_boosting_clf.fit(x_train, y_train.squeeze())
 
-    return random_forest_clf.predict(x_test)
+    return gradient_boosting_clf.predict(x_test)
 
 
 if __name__ == '__main__':
     __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
     csv_path_1 = os.path.join(__location__, 'data_2.csv')  # data path
 
-    wandb.init(project="mu-domaci-random-forest-and-gradient-boosting", config=os.path.join(__location__, 'random-forest-config-defaults.yaml'), mode='disabled')
+    wandb.init(project="mu-domaci-random-forest-and-gradient-boosting", config=os.path.join(__location__, 'gradient-boosting-config-defaults.yaml'), mode='disabled')
 
     x, y = load_data(csv_path_1)  # load data
 
     kwargs = {'n_estimators': wandb.config.n_estimators,
               'max_depth': wandb.config.max_depth,
-              'max_features': wandb.config.max_features,
+              'learning_rate': wandb.config.learning_rate,
               'random_state': wandb.config.random_state}
 
     accuracies = repeated_k_fold(x, y,
-                                 train_and_predict_foo=lambda x_train, y_train, x_test: train_and_predict_foo(x_train, y_train, x_test, **kwargs),
+                                 train_and_predict_foo=lambda x_train, y_train, x_test: train_and_predict_foo(x_train, y_train, x_test,
+                                                                                                              n_estimators=wandb.config.n_estimators,
+                                                                                                              max_depth=wandb.config.max_depth,
+                                                                                                              learning_rate=wandb.config.learning_rate,
+                                                                                                              random_state=wandb.config.random_state),
                                  metric_foo=accuracy_score, n_splits=5, n_repeats=2, random_state=wandb.config.random_state)
 
     print(pd.DataFrame(accuracies).describe())
--- izbor_prediktora_stabla_ansambli/hyperparameter_sweep.py
+++ izbor_prediktora_stabla_ansambli/hyperparameter_sweep.py
@@ -8,11 +8,11 @@ import numpy as np
 from sklearn.metrics import accuracy_score
 
 from data_loading.data_loading import load_data
-from izbor_prediktora_stabla_ansambli.random_forest import train_and_predict_foo
+from izbor_prediktora_stabla_ansambli import random_forest, gradient_boosting
 from utils.validation import repeated_k_fold
 
 
-def train(x, y, config_path, n_splits=5, n_repeats=2):
+def train(x, y, config_path, train_and_predict_foo, n_splits=5, n_repeats=2):
     """
 
     :param x: np.ndarray; shape num_samples x num_features; dataset feature matrix
@@ -23,13 +23,18 @@ def train(x, y, config_path, n_splits=5, n_repeats=2):
     :return:
     """
     wandb.init(config=config_path)
+    kwargs = {'n_estimators': wandb.config.n_estimators,
+              'max_depth': wandb.config.max_depth,
+              'random_state': wandb.config.random_state}
+
+    if hasattr(wandb.config, 'learning_rate'):
+        kwargs['learning_rate'] = wandb.config.learning_rate
+
+    if hasattr(wandb.config, 'max_features'):
+        kwargs['max_features'] = wandb.config.max_features
 
     accuracies = repeated_k_fold(x, y,
-                                 train_and_predict_foo=lambda x_train, y_train, x_test: train_and_predict_foo(x_train, y_train, x_test,
-                                                                                                              n_estimators=wandb.config.n_estimators,
-                                                                                                              max_depth=wandb.config.max_depth,
-                                                                                                              max_features=wandb.config.max_features,
-                                                                                                              random_state=wandb.config.random_state),
+                                 train_and_predict_foo=lambda x_train, y_train, x_test: train_and_predict_foo(x_train, y_train, x_test, **kwargs),
                                  metric_foo=accuracy_score, n_splits=n_splits, n_repeats=n_repeats, random_state=wandb.config.random_state)
 
     wandb.log({'accuracy': np.mean(accuracies)})
@@ -46,10 +51,11 @@ if __name__ == '__main__':
     if args.classifier_name.lower() == 'random_forest':
         sweep_config_name = 'random-forest-sweep-config.yaml'
         defaults_config_name = 'random-forest-config-defaults.yaml'
-    elif args.classifier_name.lower() == 'gradient_boost':
-        raise NotImplemented
-        # sweep_config_name = ''
-        # defaults_config_name = ''
+
+    elif args.classifier_name.lower() == 'gradient_boosting':
+        sweep_config_name = 'gradient-boosting-sweep-config.yaml'
+        defaults_config_name = 'gradient-boosting-config-defaults.yaml'
+
     else:
         raise ValueError("Classifier '{:s}' is not supported".format(args.classifier_name.lower()))
 
@@ -67,14 +73,14 @@ if __name__ == '__main__':
 
 
     def train_random_forest():
-        train(x, y, config_path=os.path.join(__location__, defaults_config_name), n_splits=5, n_repeats=2)
+        train(x, y, config_path=os.path.join(__location__, defaults_config_name), train_and_predict_foo=random_forest.train_and_predict_foo, n_splits=5, n_repeats=2)
 
 
     def train_gradient_boost():
-        raise NotImplemented
+        train(x, y, config_path=os.path.join(__location__, defaults_config_name), train_and_predict_foo=gradient_boosting.train_and_predict_foo, n_splits=5, n_repeats=2)
 
 
     train_foo = train_random_forest if args.classifier_name.lower() == 'random_forest' else train_gradient_boost
 
-    sweep_id = wandb.sweep(sweep_config, project="mu-domaci-random-forest-and-gradient-boost")
+    sweep_id = wandb.sweep(sweep_config, project="mu-domaci-random-forest-and-gradient-boosting")
     wandb.agent(sweep_id, function=train_foo, count=args.count)
