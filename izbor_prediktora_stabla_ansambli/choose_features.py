9181f10f25318cab3ede735c63a14aab1b10ffec
Viktor <todosijevicviktor998@gmail.com>
2022-12-11 13:22:11 +0100

    plot the accuracies of selected features for corrcoef as well

--- izbor_prediktora_stabla_ansambli/choose_features.py
+++ izbor_prediktora_stabla_ansambli/choose_features.py
@@ -56,6 +56,33 @@ def forward_selection(features, targets, train_and_predict_foo, metric_foo, num_
     return chosen_feature_ids, best_scores
 
 
+def get_selection_ticks(selected_features):
+    """
+    Get the subsets in string form
+    :param selected_features: List[int]; list of selected features
+    :return: List[str]
+    """
+    ticks = []  # x tick labels in the form of feature id subsets
+    for i in range(len(selected_features)):
+        ticks.append(str(selected_features[0:i + 1]).replace("[", "").replace("]", ""))
+
+    return ticks
+
+
+def plot_selection(selected_features, scores, colors, rotation=30, linestyles=':', alpha=1.0, marker='^', markersize=16):
+    ticks = get_selection_ticks(selected_features)
+
+    fig = plt.figure()  # plot the selection results
+    x_axis = np.arange(len(selected_features))
+    plt.xticks(x_axis, ticks, ha="right", size='xx-small', rotation=rotation)  # {'xx-small', 'x-small', 'small', 'medium', 'large', 'x-large', 'xx-large'}
+    for i in range(len(selected_features)):
+        plt.vlines(x_axis[i], min(scores), scores[i], colors=colors[selected_features[i], :], linestyles=linestyles, alpha=alpha)
+        plt.plot(x_axis[i], scores[i], c=colors[selected_features[i], :], marker=marker, markersize=markersize)
+    plt.xlabel('kombinacija prediktora')
+    plt.ylabel('tačnost')
+    fig.tight_layout()
+
+
 if __name__ == '__main__':
     __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
     csv_path_1 = os.path.join(__location__, 'data_1.csv')  # data path
@@ -73,14 +100,14 @@ if __name__ == '__main__':
 
     # get correlation coefficients and sort them
     abs_corr_coefs = np.abs(np.array([corrcoef(x[:, i], y) for i in range(num_features)]))
-    sorted_ids = np.argsort(abs_corr_coefs)
+    corr_sorted_ids = np.argsort(abs_corr_coefs)
 
     plt.figure()  # plot correlation coefficients
     x_axis = np.arange(num_features)[::-1]
-    plt.xticks(x_axis, sorted_ids)
+    plt.xticks(x_axis, corr_sorted_ids)
     for i in range(num_features):
-        plt.vlines(x_axis[i], min(abs_corr_coefs), abs_corr_coefs[sorted_ids[i]], colors=colors[sorted_ids[i], :], linestyles=stem_linestyle, alpha=stem_alpha)
-        plt.plot(x_axis[i], abs_corr_coefs[sorted_ids[i]], marker=marker, c=colors[sorted_ids[i], :], markersize=markersize)
+        plt.vlines(x_axis[i], min(abs_corr_coefs), abs_corr_coefs[corr_sorted_ids[i]], colors=colors[corr_sorted_ids[i], :], linestyles=stem_linestyle, alpha=stem_alpha)
+        plt.plot(x_axis[i], abs_corr_coefs[corr_sorted_ids[i]], marker=marker, c=colors[corr_sorted_ids[i], :], markersize=markersize)
 
     plt.xlabel('redni broj prediktora')
     plt.ylabel(r'$|\rho(x_i, y)|$')
@@ -98,22 +125,18 @@ if __name__ == '__main__':
         return train_and_predict(x_train, y_train, x_test, svm_solver_type=svm_solver_type, C=C, sigma=sigma)
 
 
+    accuracies_corr = []
+    best_corr_ids = corr_sorted_ids[::-1]
+    for i in tqdm(range(len(corr_sorted_ids))):
+        features = x[:, best_corr_ids[0:i + 1]]
+        accuracies = repeated_k_fold(features, svm_labels, train_and_predict_foo, metric_foo=accuracy_score, n_splits=5, n_repeats=1)
+        accuracies_corr.append(np.mean(accuracies))
+
+    plot_selection(list(best_corr_ids), accuracies_corr, colors, rotation=30, linestyles=stem_linestyle, alpha=stem_alpha, marker=marker, markersize=markersize)
+
     # select features using a wrapper algorithm with a linear SVM classifier
-    selected_features, best_scores = forward_selection(x, svm_labels, train_and_predict_foo=train_and_predict_foo, metric_foo=accuracy_score,
-                                                       num_features=13, n_splits=5, n_repeats=1)
+    wrapper_selected_features, accuracies_wrapper = forward_selection(x, svm_labels, train_and_predict_foo=train_and_predict_foo, metric_foo=accuracy_score,
+                                                                      num_features=13, n_splits=5, n_repeats=1)
 
-    ticks = []  # x tick labels in the form of feature id subsets
-    for i in range(len(selected_features)):
-        ticks.append(str(selected_features[0:i + 1]).replace("[", "").replace("]", ""))
-
-    fig = plt.figure()  # plot the selection results
-    x_axis = np.arange(len(selected_features))
-    plt.xticks(x_axis, ticks, rotation=30, ha="right", size='xx-small')  # {'xx-small', 'x-small', 'small', 'medium', 'large', 'x-large', 'xx-large'}
-    for i in range(len(selected_features)):
-        plt.vlines(x_axis[i], min(best_scores), best_scores[i], colors=colors[selected_features[i], :], linestyles=stem_linestyle, alpha=stem_alpha)
-        plt.plot(x_axis[i], best_scores[i], marker=marker, c=colors[selected_features[i], :], markersize=markersize)
-
-    plt.xlabel('kombinacija prediktora')
-    plt.ylabel('tačnost')
-    fig.tight_layout()
+    plot_selection(wrapper_selected_features, accuracies_wrapper, colors, rotation=30, linestyles=stem_linestyle, alpha=stem_alpha, marker=marker, markersize=markersize)
     plt.show()
