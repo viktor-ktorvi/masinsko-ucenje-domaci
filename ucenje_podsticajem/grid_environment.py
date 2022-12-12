2f13465b400b5af0072928b282b811d2aa7af273
Viktor <todosijevicviktor998@gmail.com>
2022-12-12 23:59:35 +0100

    apply action + pretty printMap

--- ucenje_podsticajem/grid_environment.py
+++ ucenje_podsticajem/grid_environment.py
@@ -1,5 +1,7 @@
 from enum import IntEnum
 
+from ucenje_podsticajem.actor import ActionTypes
+
 
 class FieldTypes(IntEnum):
     WALL = 0
@@ -9,15 +11,32 @@ class FieldTypes(IntEnum):
 
 class State2D:
     def __init__(self, row=0, column=0, row_limits=None, column_limits=None):
+        """
+        Init a state that consists of (row, column).
+        :param row: int
+        :param column: int
+        :param row_limits: tuple(int, int)
+        :param column_limits: tuple(int, int)
+        """
         self.row = row
         self.column = column
         self.row_limits = row_limits
         self.column_limits = column_limits
 
     def inLimits(self):
+        """
+        Check if the state is within the limits.
+        :return: boolean
+        """
         return self.row_limits[0] <= self.row <= self.row_limits[1] and self.column_limits[0] <= self.column <= self.column_limits[1]
 
     def isEqual(self, row, column):
+        """
+        Check if the state is equal to a row column pair.
+        :param row: int
+        :param column: int
+        :return: boolean
+        """
         return row == self.row and column == self.column
 
 
@@ -31,7 +50,7 @@ class GridEnvironment:
         self.current_state = None
         self.start_state = None
 
-        if env_map is None:
+        if env_map is None:  # default map
             self.env_map = [
                 [FieldTypes.WALL] * 7,
                 [FieldTypes.WALL] + [FieldTypes.NEUTRAL] * 5 + [FieldTypes.WALL],
@@ -45,6 +64,8 @@ class GridEnvironment:
 
         self.height = len(self.env_map)
         self.width = len(self.env_map[0])
+        self.row_limits = (1, self.height - 2)
+        self.column_limits = (1, self.width - 2)
 
         self.reset(start_state)
 
@@ -55,32 +76,70 @@ class GridEnvironment:
         :return:
         """
         if start_state is None:
-            self.start_state = State2D(1, 1, (1, self.height - 1), (1, self.width - 1))
+            self.start_state = State2D(1, 1, self.row_limits, self.column_limits)
 
         assert self.start_state.inLimits()  # assert that the state is in bounds
 
         self.current_state = self.start_state
 
     def step(self, action):
+        # TODO
+        #  stochastically change the action
+        #  apply action
+        #  return the reward and observation(current state)
         raise NotImplemented
 
-    def printMap(self, step=0, cell_size=10):
+    def applyAction(self, action):
+        """
+        Apply the action to the environment and update the current state.
+        :param action: ActionTypes
+        :return:
+        """
+        row, column = self.current_state.row, self.current_state.column
+
+        if action == ActionTypes.UP:
+            row -= 1
+        elif action == ActionTypes.DOWN:
+            row += 1
+        elif action == ActionTypes.LEFT:
+            column -= 1
+        elif action == ActionTypes.RIGHT:
+            column += 1
+        else:
+            raise ValueError("Action '{:s}' not supported.".format(action))
+
+        new_state = State2D(row, column, self.row_limits, self.column_limits)
+
+        if new_state.inLimits():  # if the new state is within limits then update the current state
+            self.current_state = new_state
+
+    def printMap(self, step=0, action=None, cell_size=10):
         """
         Print the map and the current state denoted by a '*'.
         :return:
         """
-        print("-" * cell_size * (len(self.env_map[0]) + 1), end=' step = ' + str(step) + '\n')
+
+        # print delimiter and timestep
+        print("{:s} {:s}".format("-" * cell_size * (len(self.env_map[0]) + 1),
+                                 "step = {:d}, ".format(step)), end='')
+
+        # action taken
+        if hasattr(action, 'name'):
+            print("action = {:s}, ".format(action.name), end='')
+
+        print("state = ({:<d}, {:<d})".format(self.current_state.row, self.current_state.column))
+
         print("{:{cell_size}s}".format("", cell_size=cell_size), end='')
         for i in range(len(self.env_map[0])):
             print("{:{cell_size}s}".format(str(i), cell_size=cell_size), end='')
         print()
 
+        # print map
         for row in range(len(self.env_map)):
             print("{:{cell_size}s}".format(str(row), cell_size=cell_size), end='')
             for column in range(len(self.env_map[0])):
-
                 if self.current_state.isEqual(row, column):
-                    output = "*" + self.env_map[row][column].name
+                    output = "*" + self.env_map[row][column].name  # denote the current state with a '*'
                 else:
                     output = self.env_map[row][column].name
 
@@ -92,3 +151,8 @@ class GridEnvironment:
 if __name__ == '__main__':
     grid_environment = GridEnvironment()
     grid_environment.printMap()
+
+    actions = [ActionTypes.RIGHT, ActionTypes.UP, ActionTypes.RIGHT, ActionTypes.DOWN, ActionTypes.UP] + [ActionTypes.RIGHT] * 3 + [ActionTypes.LEFT] * 8 + [ActionTypes.DOWN] * 3
+    for i in range(len(actions)):
+        grid_environment.applyAction(actions[i])
+        grid_environment.printMap(i + 1, actions[i])
