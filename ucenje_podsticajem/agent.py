a24d0d9bfd9068db5d23f34545b6078ca2ae1064
Viktor <todosijevicviktor998@gmail.com>
2022-12-22 14:40:12 +0100

    implement REINFORCE. doesn't work

--- ucenje_podsticajem/agent.py
+++ ucenje_podsticajem/agent.py
@@ -1,6 +1,10 @@
+import random
+
 import numpy as np
 
 from enum import IntEnum
+from scipy.special import softmax
+from sklearn.preprocessing import OneHotEncoder
 
 
 class ActionTypes(IntEnum):
@@ -118,6 +122,79 @@ class AgentQ:
         return action
 
 
+class AgentREINFORCE:
+    def __init__(self, state_dimension):
+        self.theta = np.random.randn(state_dimension + len(ActionTypes), 1)
+
+        enc = OneHotEncoder()
+        self.action_encodings = enc.fit_transform(np.arange(len(ActionTypes)).reshape((len(ActionTypes), 1))).toarray()
+
+        self.action_list = [ActionTypes(i) for i in range(len(ActionTypes))]
+
+    def policy(self, state):
+        """
+        Calculate the agents' policy for the given state. Return the features as well.
+        :param state: State2D
+        :return: tuple(np.ndarray); np.ndarray; shape num_actions x 1 - policy |
+                                    np.ndarray; shape num_actions x (state_dimension + num_actions) features
+        """
+        features = self.constructFeatures(state)
+        policy = softmax(features @ self.theta)  # calculate the policy
+
+        return policy, features
+
+    def act(self, state):
+        """
+        Calculate the agents policy and sample an action from it.
+        :param state: State2D
+        :return: ActionTypes
+        """
+        policy, _ = self.policy(state)
+
+        action = random.choices(self.action_list, weights=list(policy.flatten()))[0]  # sample from the policy
+
+        return action
+
+    def score(self, state):
+        policy, features = self.policy(state)
+
+        score = features - np.sum(features * policy, axis=0)
+
+        return score
+
+    def observe(self, observations, actions, rewards, learning_rate, gamma):
+        assert len(observations) == len(actions)
+        assert len(actions) == len(rewards)
+
+        v = np.correlate(gamma ** np.arange(len(rewards)), rewards, "full")[:len(rewards)]
+
+        scores = []
+        for i in range(len(observations)):
+            scores.append(self.score(observations[i])[actions[i]].reshape(self.theta.shape))    # num_time_steps x num_features x 1
+
+        # multiply scores and values timestep wise and sum them up
+        total_update = np.sum(v[:, np.newaxis, np.newaxis] * np.array(scores), axis=0)
+
+        self.theta += learning_rate * total_update
+
+    def constructFeatures(self, state):
+        """
+        concatenate the state and action encodings
+        :param state: State2d
+        :return: np.ndarray; shape num_actions x (state_dimension + num_actions)
+        """
+        return np.hstack((np.tile(state.toArray(), (len(ActionTypes), 1)), self.action_encodings))
+
+
 if __name__ == '__main__':
-    agent = AgentQ(4, 7)
-    print(agent.Q_table.shape)
+    agentQ = AgentQ(4, 7)
+    print(agentQ.Q_table.shape)
+
+    agentR = AgentREINFORCE(2)
+
+    from ucenje_podsticajem.grid_environment import State2D
+
+    state = State2D(1, 3)
+    agentR.act(state)
+
+    debug_var = None
---
+++ ucenje_podsticajem/reinforce_simulation.py
@@ -0,0 +1,49 @@
+from tqdm import tqdm
+
+from matplotlib import pyplot as plt
+
+from ucenje_podsticajem.agent import AgentREINFORCE
+from ucenje_podsticajem.grid_environment import GridEnvironment
+from utils.utils import running_average
+
+if __name__ == '__main__':
+    num_episodes = 1000
+    learning_rate = 0.5
+    gamma = 0.9
+
+    # init env and agent
+    grid_environment = GridEnvironment()
+    agent = AgentREINFORCE(state_dimension=2)
+
+    rewards = []
+
+    for episode in tqdm(range(num_episodes)):
+        grid_environment.reset()  # go back to the starting state
+        observation, reward, terminate = grid_environment.step()  # initial step
+
+        episode_observations = []
+        episode_actions = []
+        episode_rewards = []
+
+        while not terminate:
+            episode_observations.append(observation)
+
+            action = agent.act(observation)
+
+            observation, reward, terminate = grid_environment.step(action)
+
+            episode_actions.append(action)
+            episode_rewards.append(reward)
+
+        agent.observe(episode_observations, episode_actions, episode_rewards, learning_rate, gamma)
+
+        rewards.append(reward)
+
+    plt.figure()
+    plt.plot(rewards, label='nagrada')
+    plt.plot(running_average(rewards, 40), label='srednja nagrada')
+    plt.xlabel('epizoda')
+    plt.ylabel('R')
+    plt.legend()
+
+    plt.show()
